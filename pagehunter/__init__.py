c48ef60a4476131a1dca3084dfef776f2f92253c
v1ll4n <v1ll4n@villanch.top>
2018-09-10 11:25:24 +0800

    [feature]: add setup.py

---
+++ pagehunter/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# coding:utf-8
+from .core import Page, PageHunter
+from . import utils
+
+__all__ = [
+    "Page", "PageHunter", "utils",
+]
---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# coding:utf-8
+import subprocess
+from setuptools import setup, find_packages
+
+
+def get_git_revision_short_hash():
+    return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'])
+
+
+setup(
+    name='arupy',
+    # See https://www.python.org/dev/peps/pep-0440/
+    version="1.{}".format(get_git_revision_short_hash()),
+    description='PageHunter - Web 页面的相似度处理 from sqlmap',
+    author='v1ll4n',
+    author_email='v1ll4n@qq.com',
+    url='http://pagehunter.com',
+    install_requires=[],
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+)
