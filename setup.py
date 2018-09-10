1d77747cfdc8d2ed2e441602b2faf5f845391590
v1ll4n <v1ll4n@villanch.top>
2018-09-10 11:30:12 +0800

    fix bug

--- setup.py
+++ setup.py
@@ -5,7 +5,10 @@ from setuptools import setup, find_packages
 
 
 def get_git_revision_short_hash():
-    return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'])
+    try:
+        return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'])
+    except:
+        return "expr"
 
 
 setup(
