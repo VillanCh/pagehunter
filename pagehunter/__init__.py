48227e426254288138bae4cd8bd9ef88b3cd918a
v1ll4n <v1ll4n@villanch.top>
2018-09-13 21:20:39 +0800

    [feature]: remove fickle, see: VillanCh/fickle

--- pagehunter/__init__.py
+++ pagehunter/__init__.py
@@ -2,8 +2,7 @@
 # coding:utf-8
 from .core import Page, PageHunter
 from . import utils
-from . import fickle
 
 __all__ = [
-    "Page", "PageHunter", "utils", "fickle",
+    "Page", "PageHunter", "utils",
 ]
